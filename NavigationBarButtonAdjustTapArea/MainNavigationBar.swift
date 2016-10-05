f013cb865cbbbb4f9b5b2451f0aa057a64da8f10
Takuya ASO <milanista224@me.com>
2016-10-06 01:03:50 +0900

    [Swift3.0]Add UINavigationBar SubClass.

---
+++ NavigationBarButtonAdjustTapArea/MainNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  MainNavigationBar.swift
+//  NavigationBarButtonAdjustTapArea
+//
+//  Created by 麻生 拓弥 on 2016/10/06.
+//  Copyright © 2016年 麻生 拓弥. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationBar: UINavigationBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
